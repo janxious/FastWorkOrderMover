dd798017385727230c4473175469ce1793680cf0
gnivler <gnivil@gmail.com>
2018-08-03 18:09:48 -0600

    partially implemented

--- FastWorkOrderMove.cs
+++ FastWorkOrderMove.cs
@@ -77,4 +77,59 @@ namespace FastWorkOrderMover
             return false;
         }
     }
+
+    [HarmonyPatch(typeof(TaskManagementWidget), "OnSortDown")]
+    public static class TaskManagementWidget_OnSortDown_Patch2
+    {
+        public static bool Prefix(TaskManagementElement element, TaskManagementWidget __instance,
+            ref List<TaskManagementElement> ___allMechElements, ref List<WorkOrderEntry> ___mechLabQueue)
+        {
+            var ctrlHeld = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+            var instance = Traverse.Create(__instance);
+            if (ctrlHeld)
+            {
+                var sortingDic = new SortedDictionary<int, TaskManagementElement>();
+                var joinedDic = new SortedDictionary<WorkOrderEntry, TaskManagementElement>();
+                var workQueue = new List<WorkOrderEntry>();
+
+                // populate the SortedDictionary with an int for number of days, and the element as value
+                foreach (var kvp in ___allMechElements)
+                {
+                    sortingDic.Add(kvp.cumulativeDaysRemaining, kvp);
+                    Logger.Debug($"Added {kvp.name} with {kvp.cumulativeDaysRemaining} days");
+                }
+
+                // populate another dictionary with the element and the workorder
+                foreach (var kvp in sortingDic)
+                {
+                    joinedDic.Add(kvp.Value.Entry, kvp.Value);
+                    Logger.Debug($"Added\n{kvp.Value.Entry}\n{kvp.Value}");
+                }
+
+                ___allMechElements.Clear();
+                ___mechLabQueue.Clear();
+                int x = 0;
+                foreach (var kvp in joinedDic)
+                {
+                    element.transform.SetSiblingIndex(x++);
+                    ___allMechElements.Add(kvp.Value);
+                    Logger.Debug($"Added {kvp.Value}");
+                    ___allMechElements.Reverse();
+
+                    ___mechLabQueue.Add(kvp.Key);
+                    Logger.Debug($"Added {kvp.Key}");
+                    ___mechLabQueue.Reverse();
+                }
+
+                Logger.Debug(___mechLabQueue.Count + " " + ___allMechElements);
+                foreach (var entry in ___mechLabQueue)
+                {
+                    Logger.Debug($"entry: {entry.ToString()}");
+                }
+            }
+
+            instance.Field("modified").SetValue(true);
+            return false;
+        }
+    }
 }
