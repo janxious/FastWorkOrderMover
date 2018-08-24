e937c30989fb29e5734163a47084ca3df476aab3
gnivler <gnivil@gmail.com>
2018-08-23 20:42:31 -0600

    working down

--- FastWorkOrderMove.cs
+++ FastWorkOrderMove.cs
@@ -8,6 +8,7 @@ using Harmony;
 using Newtonsoft.Json;
 using UIWidgets;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace FastWorkOrderMover
 {
@@ -34,121 +35,122 @@ namespace FastWorkOrderMover
         }
     }
 
-    [HarmonyPatch(typeof(TaskManagementWidget), "OnSortUp")]
-    public static class TaskManagementWidget_OnSortUp_Patch
-    {
-        public static bool Prefix(TaskManagementElement element, TaskManagementWidget __instance)
-        {
-            var shiftHeld = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-            var magicThis = Traverse.Create(__instance);
-            var mechElements = magicThis.Field("allMechElements").GetValue<List<TaskManagementElement>>();
-            var initialIndex = mechElements.IndexOf(element);
-            if (initialIndex == 0)
-            {
-                return false;
-            }
+    ///  [HarmonyPatch(typeof(TaskManagementWidget), "OnSortUp")]
+    ///  public static class TaskManagementWidget_OnSortUp_Patch
+    ///  {
+    ///      public static bool Prefix(TaskManagementElement element, TaskManagementWidget __instance)
+    ///      {
+    ///          var shiftHeld = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+    ///          var magicThis = Traverse.Create(__instance);
+    ///          var mechElements = magicThis.Field("allMechElements").GetValue<List<TaskManagementElement>>();
+    ///          var initialIndex = mechElements.IndexOf(element);
+    ///          if (initialIndex == 0)
+    ///          {
+    ///              return false;
+    ///          }
+    ///          var mechLabQueue = magicThis.Field("mechLabQueue").GetValue<List<WorkOrderEntry>>();
+    ///          var newIndex = shiftHeld ? 0 : initialIndex - 1;
+    ///          mechElements.Remove(element);
+    ///          mechElements.Insert(newIndex, element);
+    ///          mechLabQueue.Remove(element.Entry);
+    ///          mechLabQueue.Insert(newIndex, element.Entry);
+    ///          element.transform.SetSiblingIndex(newIndex);
+    ///          magicThis.Field("modified").SetValue(true);
+    ///          return false;
+    ///      }
+    ///  }
 
-            var mechLabQueue = magicThis.Field("mechLabQueue").GetValue<List<WorkOrderEntry>>();
-            var newIndex = shiftHeld ? 0 : initialIndex - 1;
-            mechElements.Remove(element);
-            mechElements.Insert(newIndex, element);
-            mechLabQueue.Remove(element.Entry);
-            mechLabQueue.Insert(newIndex, element.Entry);
-            element.transform.SetSiblingIndex(newIndex);
-            magicThis.Field("modified").SetValue(true);
-            return false;
-        }
-    }
-
-/*
-    [HarmonyPatch(typeof(TaskManagementWidget), "OnSortDown")]
-    public static class TaskManagementWidget_OnSortDown_Patch
-    {
-        public static bool Prefix(TaskManagementElement element, TaskManagementWidget __instance)
-        {
-            var shiftHeld = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-            var magicThis = Traverse.Create(__instance);
-            var mechElements = magicThis.Field("allMechElements").GetValue<List<TaskManagementElement>>();
-            var initialIndex = mechElements.IndexOf(element);
-            if (initialIndex >= mechElements.Count) { return false; }
-
-            var mechLabQueue = magicThis.Field("mechLabQueue").GetValue<List<WorkOrderEntry>>();
-            var newIndex = shiftHeld ? mechElements.Count - 1 : initialIndex + 1;
-            mechElements.Remove(element);
-            mechElements.Insert(newIndex, element);
-            mechLabQueue.Remove(element.Entry);
-            mechLabQueue.Insert(newIndex, element.Entry);
-            element.transform.SetSiblingIndex(newIndex);
-            magicThis.Field("modified").SetValue(true);
-            return false;
-        }
-    }
-*/
+//    [HarmonyPatch(typeof(TaskManagementWidget), "OnSortDown")]
+//    public static class TaskManagementWidget_OnSortDown_Patch
+//    {
+//        public static bool Prefix(TaskManagementElement element, TaskManagementWidget __instance)
+//        {
+//            var shiftHeld = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+//            var magicThis = Traverse.Create(__instance);
+//            var mechElements = magicThis.Field("allMechElements").GetValue<List<TaskManagementElement>>();
+//            var initialIndex = mechElements.IndexOf(element);
+//            if (initialIndex >= mechElements.Count) { return false; }
+//
+//            var mechLabQueue = magicThis.Field("mechLabQueue").GetValue<List<WorkOrderEntry>>();
+//            var newIndex = shiftHeld ? mechElements.Count - 1 : initialIndex + 1;
+//            mechElements.Remove(element);
+//            mechElements.Insert(newIndex, element);
+//            mechLabQueue.Remove(element.Entry);
+//            mechLabQueue.Insert(newIndex, element.Entry);
+//            element.transform.SetSiblingIndex(newIndex);
+//            magicThis.Field("modified").SetValue(true);
+//            return false;
+//        }
+//    }
     [HarmonyPatch(typeof(TaskManagementWidget), "OnSortDown")]
     public static class TaskManagementWidget_OnSortDown_Patch2
     {
-        public static bool Prefix(TaskManagementElement element, TaskManagementWidget __instance,
-            ref List<TaskManagementElement> ___allMechElements, ref List<WorkOrderEntry> ___mechLabQueue)
+        // public void SetData(SimGameState sim, UnityAction closeCallback)
+
+        public static bool Prefix(
+            TaskManagementWidget __instance,
+            ref List<TaskManagementElement> ___allMechElements,
+            ref List<WorkOrderEntry> ___mechLabQueue,
+            ref bool ___modified)
+        {
+            var sim = Traverse.Create(__instance).Field("Sim").GetValue<SimGameState>();
+            var ctrlHeld = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+            if (!ctrlHeld) return true;
+
+            // TaskMangementElement is available through allMechElements and contains both the days remaining and mech work queue item
+            var widgetElements = ___allMechElements.OrderByDescending(m => m.cumulativeDaysRemaining).ThenBy(m => m.Entry.GUID).ToList();
+            var mechLabElements = ___allMechElements.OrderByDescending(x => x.cumulativeDaysRemaining).ThenBy(x => x.Entry.GUID).Select(x => x.Entry).ToList();
+
+            // this appears to be the only thing required in the other patches?
+            ___modified = true;
+            Logger.Debug("Modified");
+
+            sim.MechLabQueue = mechLabElements;
+            
+            __instance.OnPooled();
+            Logger.Debug("OnPooled");
+
+            // this emulates code found in the assembly plus this class has a field closeCallback so...?
+            __instance.SetData(sim, __instance.closeCallback);
+            Logger.Debug("SetData");
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(TaskManagementWidget), "OnSortUp")]
+    public static class TaskManagementWidget_OnSortUp_Patch2
+    {
+        public static bool Prefix(
+            TaskManagementWidget __instance,
+            ref List<TaskManagementElement> ___allMechElements,
+            ref List<WorkOrderEntry> ___mechLabQueue,
+            ref bool ___modified)
         {
             var ctrlHeld = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
-            var instance = Traverse.Create(__instance);
-            if (!ctrlHeld) return true;        
+            if (!ctrlHeld) return true;
 
-            // zany approach using two sorted dictionaries
-            var sortingDic = new SortedDictionary<int, TaskManagementElement>();
-            var joinedDic = new SortedDictionary<WorkOrderEntry, TaskManagementElement>();
-            var workQueue = new List<WorkOrderEntry>();
+            var sim = Traverse.Create(__instance).Field("Sim").GetValue<SimGameState>();
 
-            // populate the SortedDictionary with an int for number of days, and the element as value
-            foreach (var kvp in ___allMechElements)
-            {
-                sortingDic.Add(kvp.cumulativeDaysRemaining, kvp);
+            // TaskMangementElement is available through allMechElements and contains both the days remaining and mech work queue item
+            sim.MechLabQueue = ___allMechElements
+                .OrderByDescending(x => x.cumulativeDaysRemaining)
+                .ThenBy(x => x.Entry.GUID).Select(x => x.Entry).ToList();
+            Logger.Debug("Sorted");
 
-                workQueue.Sort();
-                Logger.Debug($"Added {kvp.name} with {kvp.cumulativeDaysRemaining} days");
-            }
+            //sim.MechLabQueue = mechLabElements;
+            
+            // clears the widget
+            ___modified = true;
+            Logger.Debug("Modified");
+            __instance.OnPooled();
+            Logger.Debug("OnPooled");
 
-            // populate another dictionary with the element and the workorder
-            foreach (var kvp in sortingDic)
-            {
-                joinedDic.Add(kvp.Value.Entry, kvp.Value);
-                Logger.Debug($"sortingDic Added\n{kvp.Value.Entry}\n{kvp.Value}");
-            }
+            // populates the widget with the wrong? data
+            __instance.SetData(sim, __instance.closeCallback);
+            Logger.Debug("SetData");
 
-            Logger.Debug($"joinedDic count is {joinedDic.Count}");
-            var elementHolder = new List<TaskManagementElement>();
-            var workOrderHolder = new List<WorkOrderEntry>();
-
-            ___allMechElements.Clear();
-            ___mechLabQueue.Clear();
-            Logger.Debug($"Cleared lists");
-
-            foreach (var kvp in joinedDic)
-            {
-                elementHolder.Add(kvp.Value);
-                Logger.Debug($"Added {kvp.Value}");
-
-                workOrderHolder.Add(kvp.Key);
-                Logger.Debug($"Added {kvp.Key}");
-            }
-
-            elementHolder.ForEach(x => Logger.Debug($"element: {x}"));
-            workOrderHolder.ForEach(x => Logger.Debug($"workorder: {x}"));
-
-            // lists seem to check out fine in the debugger
-            ___allMechElements = elementHolder;
-            ___mechLabQueue = workOrderHolder;
-
-            Logger.Debug(___mechLabQueue.Count + " " + ___allMechElements);
-            foreach (var entry in ___mechLabQueue)
-            {
-                Logger.Debug($"entry: {entry}");
-            }
-
-
-            // need to do something more than this perhaps?
-  
-            instance.Field("modified").SetValue(true);
+            // this appears to be the only thing required in the other patches?
             return false;
         }
     }
