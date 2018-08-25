c79098033c88b5609340b75a8c32a6ab2efe6a77
Joel Meador <joel@expectedbehavior.com>
2018-08-24 20:27:23 -0500

    refactor sort down playground

--- FastWorkOrderMove.cs
+++ FastWorkOrderMove.cs
@@ -38,43 +38,43 @@ namespace FastWorkOrderMover
     public static class TaskManagementWidget_OnSortDown_Patch
     {
         public static bool Prefix(
-            TaskManagementWidget __instance,
             TaskManagementElement element,
+            TaskManagementWidget __instance,
             ref List<TaskManagementElement> ___allMechElements,
             ref List<WorkOrderEntry> ___mechLabQueue,
-            ref bool ___modified)
+            ref bool ___modified,
+            ref UnityAction ___closeCallback,
+            ref SimGameState ___Sim)
         {
             var shiftHeld = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
             var ctrlHeld = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+            if (ctrlHeld && shiftHeld) return true;
+            if (!ctrlHeld && !shiftHeld) return true;
 
-            if (ctrlHeld && !shiftHeld)
+            if (ctrlHeld)
             {
-                var sim = Traverse.Create(__instance).Field("Sim").GetValue<SimGameState>();
-                var sortedWorkOrders = ___allMechElements.OrderByDescending(x => x.cumulativeDaysRemaining).ThenBy(x => x.Entry.GUID).Select(x => x.Entry).ToList();
-                var closeCb = Traverse.Create(__instance).Field("closeCallback").GetValue<UnityAction>();
-                Traverse.Create(sim).Property("MechLabQueue").SetValue(sortedWorkOrders);
-                __instance.SetData(sim, closeCb);
-
+                var sortedWorkOrders =
+                    ___allMechElements
+                        .OrderByDescending(el => el.cumulativeDaysRemaining)
+                        .ThenBy(el => el.Entry.GUID)
+                        .Select(el => el.Entry)
+                        .ToList();
+                AccessTools.FieldRefAccess<SimGameState, List<WorkOrderEntry>>(___Sim, "MechLabQueue") = sortedWorkOrders;
+                __instance.SetData(___Sim, ___closeCallback);
                 return false;
             }
 
-            if (!ctrlHeld && shiftHeld)
-            {
-                var initialIndex = ___allMechElements.IndexOf(element);
-                if (initialIndex >= ___allMechElements.Count) { return false; }
-                var newIndex = ___allMechElements.Count - 1;
-                ___allMechElements.Remove(element);
-                ___allMechElements.Insert(newIndex, element);
-                ___mechLabQueue.Remove(element.Entry);
-                ___mechLabQueue.Insert(newIndex, element.Entry);
-                element.transform.SetSiblingIndex(newIndex);
-                ___modified = true;
-
-                return false;
-            }
-            
-            // let original method run if no keypress modifiers were used
-            return true;
+            // shift was held
+            var initialIndex = ___allMechElements.IndexOf(element);
+            if (initialIndex >= ___allMechElements.Count) { return false; }
+            var newIndex = ___allMechElements.Count - 1;
+            ___allMechElements.Remove(element);
+            ___allMechElements.Insert(newIndex, element);
+            ___mechLabQueue.Remove(element.Entry);
+            ___mechLabQueue.Insert(newIndex, element.Entry);
+            element.transform.SetSiblingIndex(newIndex);
+            ___modified = true;
+            return false;
         }
     }
 
--- README.md
+++ README.md
@@ -7,4 +7,10 @@ BattleTech mod to make it easier to send work orders to the top or bottom
 ### How to use
 
 * Shift+click up arrow - take mech work order to the top
-* Shift+click down - take mech work order to the bottom
+* Shift+click down arrow - take mech work order to the bottom
+* Ctrl+click up arrow - order all work orders by time remaining with most time remaining at top
+* Ctrl+click down arrow - order all work orders by time remaining with least time remaining at top
+
+## Special Thanks
+
+@gnivler, HBS
